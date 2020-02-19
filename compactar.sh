01139cd2cc645f8586f76d9b583630599216919c
aluno <aluno@lab1>
2020-02-19 14:58:35 -0300

    Adicionando novos scripts.

---
+++ compactar.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+mkdir projetos
+
+cd projetos
+
+
+echo "Aula1802" > teste1.txt
+echo "Aula1802" > teste2.txt
+echo "Aula1802" > teste3.txt
+
+tar -cz projetos > projetos.tar.gz
+
+
+
+
