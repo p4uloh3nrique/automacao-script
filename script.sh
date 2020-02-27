5347cc130d2bbc92483b1f6d244b83dae019d7ac
aluno <aluno@Lab-15>
2020-02-27 11:12:29 -0300

    trabalho n1

---
+++ script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+unzip imagens-livros.zip
+
+cd imagens-livros
+
+convert *.jpg teste.png
+
+mkdir pasta-trabalho1
+
+
+mv *.png pasta-trabalho1
+
+
+zip -r pasta-trabalho1.zip pasta-trabalho1
+
+
+
+
+
