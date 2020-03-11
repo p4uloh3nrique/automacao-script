3fc363f81c9fc1a26e8e569775b6d02552e2c5ed
aluno <aluno@Lab-10>
2020-03-11 14:40:09 -0300

    adicionei comentários b

--- convert.sh
+++ convert.sh
@@ -2,12 +2,13 @@
 
 cd trabalhon1
 
+#descompactando 
 unzip -q imagens-livros.zip
-
+#compactando
 cd imagens-livros
 
 convert *.jpg teste.png
-
+#diretório
 mkdir convertidas
 
 mv teste*.png convertidas
